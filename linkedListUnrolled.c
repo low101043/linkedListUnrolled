7520447a9cb9bafca9fcb180ced6b3cefff428b2
Nathaniel Lowis <nathaniel@lowis.co.uk>
2020-11-24 19:32:03 +0000

    Added appending and inserting at index

--- linkedListUnrolled.c
+++ linkedListUnrolled.c
@@ -33,15 +33,40 @@ LinkedList* append(LinkedList* list, int num){
         //printf("Data added: %d", headNode->data[0]);
         return list;
     }
-    else{
+
+    else
+    {
+        while (node ->next != NULL)
+        {
+            node = node -> next;
+        }
+        
+        if (node->numberOfElements == MAXSIZE)
+        {
+            Node* newNode = createNode(num);
+            node->next = newNode;
+            return list;
+        }
+        else
+        {
+            node->data[node->numberOfElements] = num;
+            node->numberOfElements = (node->numberOfElements) + 1;
+            return list;
+        }
+        
+    }
+    
+    /*else{           //CHANGE TO WHILE NEXT NOT NULL
         int amountInHead;
         //printf("We have got here\n");
         amountInHead = node -> numberOfElements;
+        //printf("Data in node: %d\n", amountInHead);
         //printf("Got the data\n");
         int finished = 0;
-        while (amountInHead >= MAXSIZE && finished == 0){
+        while (amountInHead >= MAXSIZE && finished == 0 /*&& node -> next != NULL){
+            
 
-            if (node -> next== NULL){
+            if (node -> next == NULL){
                 Node* newNode = createNode(num);
                 node ->next = newNode;
                 finished = 1;
@@ -49,15 +74,20 @@ LinkedList* append(LinkedList* list, int num){
             }
             else{
                 node = node -> next;
+                amountInHead = node ->numberOfElements;
             }
         }
-        if (amountInHead < MAXSIZE){
+        if (amountInHead < MAXSIZE && node ->next == NULL){
             node -> data[amountInHead] = num;
             node -> numberOfElements = node -> numberOfElements + 1;
         }
+        else if (finished == 0){
+            printf("Not added %d\n", num);
+            //NEED TO ADD A VALUE AT END SOMEHOW!
+        }
         //printf("The data added is %d\n", node->numberOfElements);
         return list;
-    }
+    }*/
     
 }
 
@@ -66,21 +96,27 @@ void printList(LinkedList* list){
     int numOfElementsHead;
     numOfElementsHead = node -> numberOfElements;
     int iHead;
+    printf("Number of elements is: %d\n", numOfElementsHead);
     for (iHead = 0; iHead < numOfElementsHead; iHead++){
         int nodeData;
+        //printf("Number of data in node: %d\n", numOfElementsHead);
         nodeData = node ->data[iHead];
         printf("%d\n", nodeData);
     }
     //printf("We have got the node\n");
+    
     while(node->next != NULL)
     {
         node = node -> next;
         int numOfElements;
         numOfElements = node -> numberOfElements;
         int i;
+        printf("Number of elements is: %d\n", numOfElements);
         for (i = 0; i < numOfElements; i++){
             int nodeData;
+            //printf("Number of data in node: %d\n", numOfElements);
             nodeData = node ->data[i];
+
             printf("%d\n", nodeData);
         }
         
@@ -88,6 +124,125 @@ void printList(LinkedList* list){
     
     //printf("WE have got the node data %d\n", data);
     //printf("%d\n", node -> data[0]);
+}
+
+void printNode(Node* node){
+    int numOfElementsHead;
+    numOfElementsHead = node -> numberOfElements;
+    int iHead;
+    for (iHead = 0; iHead < numOfElementsHead; iHead++){
+        int nodeData;
+        nodeData = node ->data[iHead];
+        printf("%d\n", nodeData);
+    }
+}
+
+void printInList(LinkedList* list){
+    Node* head = list->head;
+
+    if (head == NULL)
+    {
+        printf("Empty List\n");
+        return;
+    }
+    else
+    {
+        while (head != NULL)
+        {
+            printf("New Node: ");
+            for (int i = 0; i < MAXSIZE; i++)
+            {
+                if (i >= head->numberOfElements)
+                {
+                    printf("NULL ->");
+                }
+                else
+                {
+                    printf("%d ->", head->data[i]);
+                }
+            }
+            head = head->next;
+        }
+        printf("End\n");
+    }
+}
+
+LinkedList* insert(int i, int value, LinkedList* list){
+
+    //NEED TO ADD ELEMENTS INTO ONE SPECIFIC PLACE
+    Node* node;
+    node = list ->head;
+    int element;
+    element = node -> numberOfElements;
+    int posInList;
+    posInList = i;
+    
+    while (node -> next != NULL && element <= i){
+        node = node -> next;
+        element = element + (node -> numberOfElements);
+        posInList = posInList - element;
+
+    }
+    printf("Index: %d, Elements Gone Through: %d, PositionToPlace: %d\n", i, element, posInList);
+    //printNode(node);
+    
+    if (node -> next == NULL){
+        Node* newNode = createNode(value);
+        node ->next = newNode;
+        return list;
+    }
+    else{
+        int elements = node ->numberOfElements;
+        if (elements == MAXSIZE){
+            
+            int dataToMoveOn;
+            //dataToMoveOn = node ->data[posInList];
+            //node -> data[posInList] = value;
+            for (int j = posInList; j < node->numberOfElements; j++){
+                int temp = node -> data[j];
+                node->data[j] = value;
+                value = temp;
+            }
+            dataToMoveOn = value;
+            int twoOn;
+            twoOn = 0;
+            while (twoOn < 1 && elements == MAXSIZE){
+                printf("In twoOn Loop.  Also Elements: %d\n", elements);
+                twoOn = twoOn + 1;
+                node = node->next;
+                for (int j = posInList; j < node->numberOfElements; j++){
+                    int temp = node -> data[j];
+                    node->data[j] = value;
+                    value = temp;
+                }
+                dataToMoveOn = value;
+                elements = node ->numberOfElements;
+                printf("New data.  twoOn %d.  elements: %d\n", twoOn, elements);
+            }
+            printf("twoOn is %d\n", twoOn);
+            if (twoOn == 1){
+                printNode(node);
+                Node* newNode = createNode(dataToMoveOn);
+                Node* nodeToReLink;
+                nodeToReLink = node -> next;
+                node -> next = newNode;
+                newNode -> next = nodeToReLink;
+            }
+            else{
+                node = node -> next;
+                printNode(node);
+            }
+        }
+    }
+    return list;
+}
+
+LinkedList* delete(int value, LinkedList* list){
+    return NULL;
+}
+
+LinkedList* deleteIndex(int index, LinkedList* list){
+    return NULL;
 }
 
 int destroyListAll(LinkedList* list){
@@ -112,24 +267,51 @@ int main(){
     //printf("List Created\n");
     
     list -> head = NULL;
-    list = append(list, 5);
+    list = append(list, 2);
+    //printList(list);
+    printf("\n\n");
     //printf("Appended 5\n");
-    list = append(list, 6);
-    //printf("Appended 6\n");
-    list = append(list, 8);
-    //printf("Appended 8\n");
-    list = append(list, 10);
-    //printf("Appended 10\n");
     list = append(list, 3);
+    //printList(list);
+    printf("\n\n");
+    //printf("Appended 6\n");
+    list = append(list, 4);
+    //printList(list);
+    printf("\n\n");
+    //printf("Appended 8\n");
+    list = append(list, 5);
+    //printList(list);
+    printf("\n\n");
+    //printf("Appended 10\n");
+    list = append(list, 6);
+    //printList(list);
+    printf("\n\n");
     //printf("Appended 3\n");
-    list = append(list, 18);
-    list = append(list, 65);
-    list = append(list, 54);
     list = append(list, 7);
-    list = append(list, 67);
+    //printList(list);
+    printf("\n\n");
+    list = append(list, 8);
+    //printList(list);
+    printf("\n\n");
+    list = append(list, 9);
+    //printList(list);
+    printf("\n\n");
+    list = append(list, 10);
+    //printList(list);
+    printf("\n\n");
+    list = append(list, 11);
+    list = append(list, 12);
+    list = append(list, 13);
+    printList(list);
+    printf("\n\n");
     //printf("Appended 18\n");
     //printf("Destroying All\n");
+    list = insert(0, 1, list);
+    list = append(list, 15);
     printList(list);
+    printf("\n\n");
+    //printList(list);
+    printInList(list);
     destroyListAll(list);
     return 0;
 }
